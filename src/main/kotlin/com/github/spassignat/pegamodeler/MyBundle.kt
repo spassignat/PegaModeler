8a11a8fc70527529de34b3ac481c7968bf0f3847
GitHub Action <action@github.com>
2023-02-04 09:28:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/spassignat/pegamodeler/MyBundle.kt
+++ src/main/kotlin/com/github/spassignat/pegamodeler/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.spassignat.pegamodeler
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/spassignat/pegamodeler/services/MyProjectService.kt
+++ src/main/kotlin/com/github/spassignat/pegamodeler/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.spassignat.pegamodeler.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.spassignat.pegamodeler.MyBundle
 
 class MyProjectService(project: Project) {
 
